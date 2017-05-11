8bed4e8c546e1f18a5d1e0736c08c7d89f69fea3
Albert <sero.albert@gmail.com>
2017-05-11 16:05:17 -0700

    Support partial

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,24 @@ var gulp = require('gulp');
 var haml = require('gulp-haml');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var injectPartials = require('gulp-inject-partials');
 
 var input = {
+  partial: __dirname + '/**/partials/_*.haml',
   haml: __dirname + '/**/*.haml',
-  scss: __dirname + '/**/*.scss'
-}
+  scss: __dirname + '/**/*.scss',
+};
 
-gulp.task('watch-haml', function() {
-  watch(input.haml, {ignoreInitial: false}).pipe(haml()).pipe(gulp.dest('./dist'));
+gulp.task('watch-haml', function () {
+  watch([input.haml, '!./node_modules/**/*', '!./dist/**/*'], {ignoreInitial: false}, function () {
+    gulp.src([input.partial, '!' + __dirname + '/node_modules/**/*', '!' + __dirname + '/dist/**/*']).pipe(haml()).pipe(gulp.dest('./dist/_tmp')).on('end', function () {
+      gulp.src([input.haml, '!' + input.partial, '!./node_modules/**/*', '!./dist/**/*']).pipe(haml()).pipe(gulp.dest('./dist/_tmp')).pipe(injectPartials()).pipe(gulp.dest('./dist'));
+    });
+  })
 });
 
-gulp.task('watch-scss', function() {
-  watch(input.scss, {ignoreInitial: false}).pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./dist'));
+gulp.task('watch-scss', function () {
+  watch([input.scss, '!./node_modules/**/*', '!./dist/**/*'], {ignoreInitial: false}).pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./dist'));
 });
 
-gulp.task('watch', ['watch-haml', 'watch-scss'])
+gulp.task('watch', ['watch-haml', 'watch-scss']);
